cbd838d30ca5c3f2f74eea5a0c049999b9534973
zhuxhss <18236571553@163.com>
2022-05-10 16:11:19 +0800

    全排列test1

--- src/wy/Solution.java
+++ src/wy/Solution.java
@@ -30,7 +30,7 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 4};
+        int[] nums = {1, 2, 5,6};
         Solution solution = new Solution();
         List<List<Integer>> list = solution.permute(nums);
         System.out.println(list);
